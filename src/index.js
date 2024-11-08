06009d3086e8974eef479cb2e346284698894eb9
ranyoung <fks1311@gmail.com>
2024-11-08 12:28:03 +0900

    라이브러리 설치 및 라우터 설정

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./pages/App";
+import { Reset } from "styled-reset";
+import { RecoilRoot } from "recoil";
+import { RouterProvider } from "react-router-dom";
+import router from "./utils/Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <Reset />
+      <RouterProvider router={router} />
+    </RecoilRoot>
   </React.StrictMode>
 );
