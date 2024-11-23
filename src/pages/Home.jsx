d22bc952877f1f8e8cd0267ebd76967755da3c1f
ranyoung <fks1311@gmail.com>
2024-11-24 00:23:01 +0900

    이미지 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,10 @@ import day6_1 from "assets/Day6_1.jpeg";
 import day6_2 from "assets/Day6_2.jpeg";
 import day6_3 from "assets/Day6_3.jpeg";
 import day6_4 from "assets/Day6_4.jpg";
-import day6_5 from "assets/Day6_5.jpeg";
+import day6_5 from "assets/Day6_5.jpg";
 import day6_6 from "assets/Day6_6.jpeg";
+import day6_7 from "assets/Day6_7.jpeg";
+
 import styled from "styled-components";
 import { useOpenContext } from "components/global/ContextProvider";
 import { NavView } from "components/global/NavView";
@@ -24,7 +26,7 @@ export const Home = () => {
     waitForAnimate: false,
     autoplay: true,
   };
-  const images = [day6_1, day6_2, day6_3, day6_4, day6_5, day6_6];
+  const images = [day6_1, day6_2, day6_3, day6_4, day6_5, day6_6, day6_7];
 
   return (
     <DefaultFrame>
