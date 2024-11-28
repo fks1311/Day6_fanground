2b09250db9e1410cd55091d8c770284966562d12
ranyoung <fks1311@gmail.com>
2024-11-28 23:13:45 +0900

    MV 컴포넌트 수정

--- src/pages/MV.jsx
+++ src/pages/MV.jsx
@@ -3,25 +3,17 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import styled from "styled-components";
 import YouTube from "react-youtube";
-import { AiOutlineAppstore, AiOutlineSplitCells } from "react-icons/ai";
+import { AiOutlineAppstore } from "react-icons/ai";
+import { BsList } from "react-icons/bs";
 import { DefaultFrame } from "components/global/DefaultFrame";
-import decodeHTMLEntities from "utils/decodeHTMLEntities";
 
 export const MV = () => {
+  const [isImg, setIsImg] = useState(true);
   const [currentId, setCurrentId] = useState(0);
-  const youtubeApiUrl = "https://www.googleapis.com/youtube/v3/search";
-  const params = {
-    key: process.env.REACT_APP_API_KEY,
-    part: "snippet",
-    q: "Day6 M/V | DAY 6 | JYP Entertainment",
-    type: "video",
-    maxResults: 50,
-  };
-
   const { isLoading, data } = useQuery({
     queryKey: ["day6"],
     queryFn: async () => {
-      const response = await axios.get(youtubeApiUrl, { params });
+      const response = await axios.get("https://fks1311.github.io/day6_cdn_data/public/mv_list.json");
       return response;
     },
   });
@@ -33,10 +25,10 @@ export const MV = () => {
       ) : (
         <Layout>
           <Video>
-            <Title>{decodeHTMLEntities(data.data.items[currentId].snippet.title)}</Title>
+            <Title>{data.data[currentId].title}</Title>
             <div style={{ borderRadius: "10px", overflow: "hidden" }}>
               <YouTube
-                videoId={data.data.items[currentId].id.videoId}
+                videoId={data.data[currentId].videoId}
                 opts={{
                   width: "1200",
                   height: "700",
@@ -45,16 +37,17 @@ export const MV = () => {
             </div>
           </Video>
           <VideoList>
-            <Icon>
-              <AiOutlineAppstore size={30} />
+            <Icon onClick={() => setIsImg(!isImg)}>
+              {isImg ? <AiOutlineAppstore size={30} /> : <BsList size={30} />}
             </Icon>
-            <Lists>
-              {data.data.items.map((data, idx) => (
-                <Item key={idx} onClick={() => setCurrentId(idx)}>
-                  <img src={data.snippet.thumbnails.default.url} />
+            <Lists $isImg={isImg} className="list">
+              {data.data.map((data, idx) => (
+                <Item key={idx} $isImg={isImg} onClick={() => setCurrentId(idx)}>
+                  <img src={`https://img.youtube.com/vi/${data.videoId}/hqdefault.jpg`} />
                   <div>
-                    <p>{decodeHTMLEntities(data.snippet.title)}</p>
-                    <span>{data.snippet.channelTitle}</span>
+                    <p>{data.title}</p>
+                    <span>{data.channelTitle}</span>
+                    <span>{data.publishTime}</span>
                   </div>
                 </Item>
               ))}
@@ -69,7 +62,8 @@ export const MV = () => {
 const Layout = styled.div`
   width: 100%;
   display: flex;
-  justify-content: space-evenly;
+  justify-content: center;
+  gap: 2rem;
   padding: 2rem;
 `;
 
@@ -101,8 +95,9 @@ const Icon = styled.div`
   padding: 1rem;
 `;
 const Lists = styled.div`
-  display: flex;
+  display: ${({ $isImg }) => ($isImg ? "flex" : "grid")};
   flex-direction: column;
+  grid-template-columns: repeat(2, 1fr);
   gap: 1rem;
   padding: 1rem;
   div {
@@ -120,14 +115,17 @@ const Lists = styled.div`
 `;
 
 const Item = styled.div`
+  flex-direction: ${({ $isImg }) => ($isImg ? "" : "column")};
   gap: 0.5rem;
   img {
+    height: 80px;
+    width: ${({ $isImg }) => ($isImg ? "120px" : "130px")};
     border-radius: 5px;
   }
   div {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
-    gap: 0.2rem;
+    // gap: 0.2rem;
   }
 `;
